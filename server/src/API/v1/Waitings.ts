7de1bb458204937afea6c90d9db9a597800eef70
M Gilang Januar <mgilangjanuar@gmail.com>
2021-09-05 11:55:31 +0700

    delete supabase interfaces

--- server/src/API/v1/Waitings.ts
+++ server/src/API/v1/Waitings.ts
@@ -1,18 +1,15 @@
 import { Request, Response } from 'express'
-import { Supabase } from '../../service/Supabase'
+import { getRepository } from 'typeorm'
+import { Waitings as Model } from '../../model/entities/Waitings'
 import { Endpoint } from '../base/Endpoint'
-import { Waitings as Model } from '../../model/Waitings'
 
 @Endpoint.API()
 export class Waitings {
 
   @Endpoint.POST('/')
-  public async create(req: Request, resp: Response): Promise<any> {
+  public async create(req: Request, res: Response): Promise<any> {
     const { email } = req.body
-    const { data: waiting, error } = await Supabase.build().from<Model>('waitings').insert([{ email }])
-    if (error) {
-      throw { status: 400, body: { error: error.message, details: error } }
-    }
-    return resp.send({ waiting })
+    const data = await getRepository<Model>(Model).save({ email: email as string })
+    return res.send({ waiting: data })
   }
 }
--- server/src/utils/FilterQuery.ts
+++ server/src/utils/FilterQuery.ts
@@ -23,3 +23,36 @@ export async function filterQuery<T = any>(base: Record<string, any>, query: Rec
   }
   return result.data
 }
+
+export function buildWhereQuery(data: Record<string, any>): string {
+  const res = Object.keys(data).reduce((res, key) => {
+    let item = ''
+    const [column, op] = key.split(/(.+)\./).slice(1)
+    if (!op) {
+      item = `${column} = '${data[key].trim()}'`
+    } else if (op === 'lt') {
+      item = `${column} < '${data[key].trim()}'`
+    } else if (op === 'lte') {
+      item = `${column} <= '${data[key].trim()}'`
+    } else if (op === 'gt') {
+      item = `${column} > '${data[key].trim()}'`
+    } else if (op === 'gte') {
+      item = `${column} >= '${data[key].trim()}'`
+    } else if (op === 'between') {
+      const [from, to] = data[key].trim().split('_')
+      item = `${column} between '${from.trim()}' and '${to.trim()}'`
+    } else {
+      item = `${column} ${op} '${data[key].trim()}'`
+    }
+    return [...res, item]
+  }, []).join(' and ')
+  console.log(res)
+  return res
+}
+
+export function buildSort(sort?: string): Record<string, any> {
+  return sort?.split(',').reduce((res, data) => {
+    const [column, order] = data.split(':')
+    return { ...res, [column]: order?.toUpperCase() || 'ASC' }
+  }, {}) || {}
+}
