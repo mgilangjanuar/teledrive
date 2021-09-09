48387209b20fbc684b5a4959d72204881d75f05f
Aqshola <aqsholafid2@gmail.com>
2021-09-09 12:04:40 +0700

    add dummy upload

--- web/src/pages/dashboard/ManageFile.tsx
+++ web/src/pages/dashboard/ManageFile.tsx
@@ -13,8 +13,7 @@ import {
 import Nav from '../common/Nav'
 import React from 'react'
 import TableFile from './components/ManageFile/TableFile'
-
-
+import FloatingBtn from './components/ManageFile/FloatingBtn'
 
 interface DataFile {
   id: string,
@@ -23,51 +22,59 @@ interface DataFile {
   mime: string,
   date: Date
 }
-interface DummyFileObj{
+interface DummyFileObj {
   [file: string]: DataFile[]
 }
 
-
-
 const dummyData: DummyFileObj = {
-  'image': [{
-    id: '1',
-    name: 'photo1.png',
-    fileSize: 2345,
-    mime: 'images/png',
-    date: new Date()
-  },{
-    id: '2',
-    name: 'photo2.png',
-    fileSize: 2345,
-    mime: 'images/jpg',
-    date: new Date()
-  }],  'video': [{
-    id: '3',
-    name: 'video1.mp4',
-    fileSize: 2345,
-    mime: 'video/mp4',
-    date: new Date()
-  },{
-    id: '4',
-    name: 'video2.mpeg',
-    fileSize: 2345,
-    mime: 'video/mpeg',
-    date: new Date()
-  },],'document': [{
-    id: '6',
-    name: 'doc1.pdf',
-    fileSize: 2345,
-    mime: 'application/pdf',
-    date: new Date()
-  }]
+  image: [
+    {
+      id: '1',
+      name: 'photo1.png',
+      fileSize: 2345,
+      mime: 'images/png',
+      date: new Date(),
+    },
+    {
+      id: '2',
+      name: 'photo2.png',
+      fileSize: 2345,
+      mime: 'images/jpg',
+      date: new Date(),
+    },
+  ],
+  video: [
+    {
+      id: '3',
+      name: 'video1.mp4',
+      fileSize: 2345,
+      mime: 'video/mp4',
+      date: new Date(),
+    },
+    {
+      id: '4',
+      name: 'video2.mpeg',
+      fileSize: 2345,
+      mime: 'video/mpeg',
+      date: new Date(),
+    },
+  ],
+  document: [
+    {
+      id: '6',
+      name: 'doc1.pdf',
+      fileSize: 2345,
+      mime: 'application/pdf',
+      date: new Date(),
+    },
+  ],
 }
 
 function ManageFile(): React.ReactElement {
-  const TabVariant={ color: 'white', bg: 'blue.400' }
+  const TabVariant = { color: 'white', bg: 'blue.400' }
   return (
     <Box bg={useColorModeValue('gray.50', 'gray.800')} minH="100vh">
-      <Container maxW="1400px" mx="auto" padding="0">
+      <Container maxW="1400px" mx="auto" padding="0" position="relative">
         <Nav />
         <Flex direction="column" alignItems="center" mt="10">
           <Input
@@ -81,26 +88,25 @@ function ManageFile(): React.ReactElement {
               <TabList>
                 <Tab _selected={TabVariant}>Images</Tab>
                 <Tab _selected={TabVariant}>Video</Tab>
-                <Tab _selected={TabVariant}>
-                  Document
-                </Tab>
+                <Tab _selected={TabVariant}>Document</Tab>
                 <Tab _selected={TabVariant}>Others</Tab>
               </TabList>
 
               <TabPanels>
-                {
-                  Object.keys(dummyData).map((key) => {
-                    console.log(dummyData[key])
-                  })
-                }
-                {dummyData && Object.keys(dummyData).map((key) => <TabPanel key={key}>
-                  <TableFile dataFile={dummyData[key]} category={key}/>
-                </TabPanel>
-                )}
+                {Object.keys(dummyData).map((key) => {
+                  console.log(dummyData[key])
+                })}
+                {dummyData &&
+                  Object.keys(dummyData).map((key,index) =>
+                    <TabPanel key={index}>
+                      <TableFile dataFile={dummyData[key]} category={key} />
+                    </TabPanel>
+                  )}
               </TabPanels>
             </Tabs>
           </Box>
         </Flex>
+        <FloatingBtn />
       </Container>
     </Box>
   )
