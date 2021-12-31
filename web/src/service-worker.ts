cc1e0149b92d36a06d38c85f6093cfa3be4a0241
M Gilang Januar <mgilangjanuar@gmail.com>
2021-12-31 11:13:14 +0700

    always not run by handler

--- web/src/service-worker.ts
+++ web/src/service-worker.ts
@@ -52,7 +52,7 @@ registerRoute(
     }
 
     // Return true to signal that we want to use the handler.
-    return true
+    return false
   },
   createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
 )
