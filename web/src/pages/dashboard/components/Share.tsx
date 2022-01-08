f8b6ba0345a69c634b1372f36b821e4c7296a0e2
M Gilang Januar <mgilangjanuar@gmail.com>
2022-01-08 21:44:06 +0700

    hotfix: share toggle

--- web/src/pages/dashboard/components/Share.tsx
+++ web/src/pages/dashboard/components/Share.tsx
@@ -61,10 +61,13 @@ const Share: React.FC<Props> = ({
     setLoadingShare(true)
     const { id, public: isPublic, sharing_options: sharingOpts, username } = formShare.getFieldsValue()
 
-    const sharing = sharingOpts?.length ? [
-      ...new Set([...sharingOpts === undefined ? sharingOptions : sharingOpts, isPublic ? '*' : null]
-        .filter(sh => isPublic ? sh : sh !== '*').filter(Boolean)) as any
+    const sharing = (sharingOpts || sharingOptions)?.length || isPublic ? [
+      ...new Set([
+        ...sharingOpts === undefined ? sharingOptions : sharingOpts,
+        isPublic ? '*' : null
+      ].filter(sh => isPublic ? sh : sh !== '*').filter(Boolean)) as any
     ] : []
+    console.log(isPublic, sharing, sharingOptions, sharingOpts)
     setSharingOptions(sharing)
 
     try {
