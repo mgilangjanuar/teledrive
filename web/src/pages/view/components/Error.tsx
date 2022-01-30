fec365d11df623cde2d3b9e3c489b932148c5b2a
M Gilang Januar <mgilangjanuar@gmail.com>
2022-01-30 13:12:11 +0700

    implement to migrate server

--- web/src/pages/view/components/Error.tsx
+++ web/src/pages/view/components/Error.tsx
@@ -1,20 +1,16 @@
 import { Button, Col, Layout, Result, Row } from 'antd'
 import React from 'react'
-import Footer from '../../components/Footer'
-import Navbar from '../../components/Navbar'
 
-const Error: React.FC<{ error: any, me: any }> = ({ error, me }) => {
+const Error: React.FC<{ error: any, me: any }> = ({ error }) => {
   return <>
-    <Navbar user={me?.user} />
     <Layout.Content className="container" style={{ minHeight: '80vh', paddingTop: '12vh' }}>
       <Row>
         <Col md={{ span: 20, offset: 2 }} span={24}>
           {error ? <Result status={[403, 404, 500].includes(error?.status) ? error?.status : 500} title={error?.data?.error || 'Something error'} extra={<Button type="primary" href="/">Home</Button>} />
-            : <Result status={404} title="File not found" extra={<Button type="primary" href="/">Home</Button>} />}
+            : <Result status={404} title="File not found" extra={<Button type="primary" href={window.location.href}>Reload</Button>} />}
         </Col>
       </Row>
     </Layout.Content>
-    <Footer me={me} />
   </>
 }
 
