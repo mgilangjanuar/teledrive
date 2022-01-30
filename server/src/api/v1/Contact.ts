f331853e0a7a42c7b97c025d8b487fe91205d124
M Gilang Januar <mgilangjanuar@gmail.com>
2022-01-30 11:13:45 +0700

    add email in contact form

--- server/src/api/v1/Contact.ts
+++ server/src/api/v1/Contact.ts
@@ -8,11 +8,11 @@ export class Contact {
 
   @Endpoint.POST()
   public async send(req: Request, res: Response): Promise<any> {
-    const { from, message } = req.body
+    const { email, from, message } = req.body
     const user = await Users.createQueryBuilder('users').select(['users.subscription_id', 'users.midtrans_id', 'users.plan']).where({ username: from }).getOne()
     await axios.post(`https://api.telegram.org/bot${process.env.TG_BOT_TOKEN}/sendMessage`, {
       chat_id: process.env.TG_BOT_OWNER_ID,
-      text: `🛎 @${from} wants to contact you!\n\n${message}\n\nfrom: \`${req.headers['cf-connecting-ip'] as string || req.ip}\` \`${req.headers['authority'] || req.headers.origin}\`${user ? `\nplan: ${user.plan}${user.subscription_id ? `\npaypal: ${user.subscription_id}` : ''}${user.midtrans_id ? `\nmidtrans: ${user.midtrans_id}` : ''}` : ''}`,
+      text: `🛎 @${from} wants to contact you!\n\n${message}\n\nfrom: \`${req.headers['cf-connecting-ip'] as string || req.ip}\`\nemail: \`${email}\`\ndomain: \`${req.headers['authority'] || req.headers.origin}\`${user ? `\nplan: ${user.plan}${user.subscription_id ? `\npaypal: ${user.subscription_id}` : ''}${user.midtrans_id ? `\nmidtrans: ${user.midtrans_id}` : ''}` : ''}`,
       parse_mode: 'Markdown'
     })
     return res.send({ success: true })
