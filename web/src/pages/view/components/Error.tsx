981d879d01f83425806cac875119f9ee56ad0b1d
M Gilang Januar <mgilangjanuar@gmail.com>
2022-01-31 20:14:27 +0700

    feature: video viewer

--- web/src/pages/view/components/Error.tsx
+++ web/src/pages/view/components/Error.tsx
@@ -6,7 +6,7 @@ const Error: React.FC<{ error: any, me: any }> = ({ error }) => {
     <Layout.Content className="container" style={{ minHeight: '80vh', paddingTop: '12vh' }}>
       <Row>
         <Col md={{ span: 20, offset: 2 }} span={24}>
-          {error ? <Result status={[403, 404, 500].includes(error?.status) ? error?.status : 500} title={error?.data?.error || 'Something error'} extra={<Button type="primary" href="/">Home</Button>} />
+          {error ? <Result status={[403, 404, 500].includes(error?.status) ? error?.status : 500} title={error?.data?.error || 'Something error'} extra={<Button type="primary" href={window.location.href}>Reload</Button>} />
             : <Result status={404} title="File not found" extra={<Button type="primary" href={window.location.href}>Reload</Button>} />}
         </Col>
       </Row>
