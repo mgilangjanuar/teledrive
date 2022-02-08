8bce095192cbfafc3cd4e3644a31f471186d5d3f
M Gilang Januar <mgilangjanuar@gmail.com>
2022-02-08 16:34:27 +0700

    refactor: minor styling

--- web/src/pages/dashboard/components/Messaging.tsx
+++ web/src/pages/dashboard/components/Messaging.tsx
@@ -605,7 +605,7 @@ const Messaging: React.FC<Props> = ({ me, collapsed, parent, setCollapsed }) => 
         </div>
       </Form.Item>
     </Layout.Footer>}
-    {collapsed && <Button shape="circle" size="large" style={{ position: 'fixed', right: 20, bottom: 30, ...collapsed ? {} : { display: 'none' } }} type="primary" icon={<CommentOutlined />} onClick={open} />}
+    {collapsed && <Button shape="circle" size="large" style={{ position: 'fixed', right: 30, bottom: 25, ...collapsed ? {} : { display: 'none' } }} type="primary" icon={<CommentOutlined />} onClick={open} />}
   </Layout.Sider>
 }
 
