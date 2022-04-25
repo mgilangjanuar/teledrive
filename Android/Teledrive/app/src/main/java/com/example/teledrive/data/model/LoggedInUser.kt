5ecb6e5e5bc06b910d9e08ae536de522899b72f4
Wizard Programming <34012548+greengeckowizard@users.noreply.github.com>
2022-04-24 21:33:37 -0500

    Rename

--- Android/Teledrive/app/src/main/java/com/example/teledrive/data/model/LoggedInUser.kt
+++
@@ -1,9 +0,0 @@
-package com.example.teledrive.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class LoggedInUser(
-    val userId: String,
-    val displayName: String
-)
