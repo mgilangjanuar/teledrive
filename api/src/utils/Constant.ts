ee7a743a65ed69708d2200625eb6e698d472fa23
M Gilang Januar <mgilangjanuar@gmail.com>
2022-05-12 10:45:12 +0700

    remove secret keys in env

--- api/src/utils/Constant.ts
+++ api/src/utils/Constant.ts
@@ -1,3 +1,6 @@
+import { randomBytes } from 'crypto'
+import { appendFileSync, existsSync, readFileSync, writeFileSync } from 'fs'
+
 export const TG_CREDS = {
   apiId: Number(process.env.TG_API_ID),
   apiHash: process.env.TG_API_HASH
@@ -9,3 +12,19 @@ export const COOKIE_AGE = 54e6
 export const CONNECTION_RETRIES = 10
 
 export const PROCESS_RETRY = 50
+
+
+// generate random secret keys
+const keys = existsSync(`${__dirname}/../../../keys`) ? readFileSync(`${__dirname}/../../../keys`, 'utf-8') : null
+const [apiSecret, filesSecret] = keys?.toString()?.split('\n') || [null, null]
+if (!process.env.API_JWT_SECRET) {
+  process.env.API_JWT_SECRET = apiSecret || randomBytes(48).toString('base64')
+  writeFileSync(`${__dirname}/../../../keys`, process.env.API_JWT_SECRET)
+}
+if (!process.env.FILES_JWT_SECRET) {
+  process.env.FILES_JWT_SECRET = filesSecret || randomBytes(48).toString('base64')
+  appendFileSync(`${__dirname}/../../../keys`, `\n${process.env.FILES_JWT_SECRET}`)
+}
+
+export const API_JWT_SECRET = process.env.API_JWT_SECRET
+export const FILES_JWT_SECRET = process.env.FILES_JWT_SECRET
