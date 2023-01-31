aa4510b6f9039920dc243192e1fde345f5588324
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-01-30 21:28:14 -0600

    Update Download.ts

--- web/src/utils/Download.ts
+++ web/src/utils/Download.ts
@@ -9,44 +9,44 @@ export async function download(id: string): Promise<ReadableStream> {
 
   const client = await telegramClient.connect()
   const readableStream = new ReadableStream({
-    start(_controller: ReadableStreamDefaultController) {
-    },
-    async pull(controller: ReadableStreamDefaultController) {
+    start(controller: ReadableStreamDefaultController) {
       console.log('start downloading:', response.files)
-      for (const file of response.files) {
-        let chat: any
-        if (file.forward_info && file.forward_info.match(/^channel\//gi)) {
-          const [type, peerId, id, accessHash] = file.forward_info.split('/')
-          let peer: Api.InputPeerChannel | Api.InputPeerUser | Api.InputPeerChat
-          if (type === 'channel') {
-            peer = new Api.InputPeerChannel({
-              channelId: BigInt(peerId) as any,
-              accessHash: BigInt(accessHash as string) as any })
-            chat = await client.invoke(new Api.channels.GetMessages({
-              channel: peer,
-              id: [new Api.InputMessageID({ id: Number(id) })]
+      (async function downloadFiles() {
+        for (const file of response.files) {
+          let chat: any
+          if (file.forward_info && file.forward_info.match(/^channel\//gi)) {
+            const [type, peerId, id, accessHash] = file.forward_info.split('/')
+            let peer: Api.InputPeerChannel | Api.InputPeerUser | Api.InputPeerChat
+            if (type === 'channel') {
+              peer = new Api.InputPeerChannel({
+                channelId: BigInt(peerId) as any,
+                accessHash: BigInt(accessHash as string) as any })
+              chat = await client.invoke(new Api.channels.GetMessages({
+                channel: peer,
+                id: [new Api.InputMessageID({ id: Number(id) })]
+              }))
+            }
+          } else {
+            chat = await client.invoke(new Api.messages.GetMessages({
+              id: [new Api.InputMessageID({ id: Number(file.message_id) })]
             }))
           }
-        } else {
-          chat = await client.invoke(new Api.messages.GetMessages({
-            id: [new Api.InputMessageID({ id: Number(file.message_id) })]
-          }))
-        }
 
-        const getData = async () => await client.downloadMedia(chat['messages'][0].media, {
-          outputFile: {
-            write: (chunk: Buffer) => {
-              if (cancel) return false
-              return controller.enqueue(chunk)
+          const media = chat['messages'][0].media;
+          await client.downloadMedia(media, {
+            outputFile: {
+              write: (chunk: Buffer) => {
+                if (cancel) return false
+                controller.enqueue(chunk)
+              },
+              close: () => controller.close()
             },
-            close: () => controller.close()
-          },
-          progressCallback: (received, total) => {
-            console.log('progress:', received, '/', total)
-          }
-        })
-        await getData()
-      }
+            progressCallback: (received, total) => {
+              console.log('progress:', received, '/', total)
+            }
+          })
+        }
+      })();
     },
     cancel() {
       cancel = true
@@ -69,5 +69,4 @@ export const directDownload = async (id: string, name: string): Promise<void> =>
     return writer.ready.then(pump)
   })
   await pump()
-
 }
