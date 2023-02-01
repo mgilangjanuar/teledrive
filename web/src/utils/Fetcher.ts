0c8db60adbeb0154b5c90c2fffdf326d70049e26
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-02-01 16:29:15 -0600

    Update Fetcher.ts

--- web/src/utils/Fetcher.ts
+++ web/src/utils/Fetcher.ts
@@ -40,38 +40,38 @@ req.interceptors.response.use(
     const { config, response: { status, data } } = error
     if (status === 401 && data?.details?.errorMessage !== 'SESSION_PASSWORD_NEEDED') {
       try {
-       await req.post('/auth/refreshToken')
-     } catch (error) {
-       return Promise.reject(error)
-     }
-     return req(config)
-   } else if (status === 429) {
-     await new Promise(resolve => setTimeout(resolve, data.retryAfter || 1000))
-     return req(config)
-   } else if (status > 500) {
-     config.headers = {
-       ...config?.headers || {},
-       'x-retry-count': config.headers['x-retry-count'] || 0
-     }
-     if (config.headers['x-retry-count'] < RETRY_COUNT) {
-       await new Promise(resolve => setTimeout(resolve, ++config.headers['x-retry-count'] * 3000))
-       return req(config)
-     }
-   }
-   return Promise.reject(error)
- }
+        await req.post('/auth/refreshToken')
+      } catch (error) {
+        return Promise.reject(error)
+      }
+      return req(config)
+    } else if (status === 429) {
+      await new Promise(resolve => setTimeout(resolve, data.retryAfter || 1000))
+      return req(config)
+    } else if (status > 500) {
+      config.headers = {
+        ...config?.headers || {},
+        'x-retry-count': config.headers['x-retry-count'] || 0
+      }
+      if (config.headers['x-retry-count'] < RETRY_COUNT) {
+        await new Promise(resolve => setTimeout(resolve, ++config.headers['x-retry-count'] * 3000))
+        return req(config)
+      }
+    }
+    return Promise.reject(error)
+  }
 )
 export const fetcher = async (url: string, authorization?: string): Promise<any> => {
- const fetch = async () => {
-   const { data } = await req.get(url, {
-     ...authorization ? { headers: { authorization: `Bearer ${authorization}` } } : {},
-     withCredentials: true
-   })
-   return data
- }
+  const fetch = async () => {
+    const { data } = await req.get(url, {
+      ...authorization ? { headers: { authorization: `Bearer ${authorization}` } } : {},
+      withCredentials: true
+    })
+    return data
+  }
   try {
-   return await fetch()
- } catch ({ response }) {
-   throw response
- }
+    return await fetch()
+  } catch ({ response }) {
+    throw response
+  }
 }
