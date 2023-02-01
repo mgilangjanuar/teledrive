153b75fc11793283212cd4503773308be95c7b73
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-02-01 16:19:55 -0600

    Update Fetcher.ts

--- web/src/utils/Fetcher.ts
+++ web/src/utils/Fetcher.ts
@@ -2,44 +2,44 @@ import axios from 'axios'
 import { RETRY_COUNT } from './Constant'
 export const apiUrl = `${localStorage.getItem('API_URL') || process.env.REACT_APP_API_URL || ''}/api/v1`
 export const req = axios.create({
- baseURL: apiUrl,
- withCredentials: true
+  baseURL: apiUrl,
+  withCredentials: true
 })
 req.interceptors.response.use(
- response => {
-   try {
-     const requests = [
-       ...JSON.parse(sessionStorage.getItem('requests') || '[]'),
-       {
-         date: new Date().toISOString(),
-         ref: location.href,
-         ...response
-       }
-     ]
-     sessionStorage.setItem('requests', JSON.stringify(requests.slice(-200)))
-   } catch (error) {
-     // ignore
-   }
-   return response
- },
- async error => {
-   try {
-     const requests = [
-       ...JSON.parse(sessionStorage.getItem('requests') || '[]'),
-       {
-         date: new Date().toISOString(),
-         ref: location.href,
-         ...error
-       }
-     ]
-     sessionStorage.setItem('requests', JSON.stringify(requests.slice(-200)))
-   } catch (error) {
-     // ignore
-   }
+  response => {
+    try {
+      const requests = [
+        ...JSON.parse(sessionStorage.getItem('requests') || '[]'),
+        {
+          date: new Date().toISOString(),
+          ref: location.href,
+          ...response
+        }
+      ]
+      sessionStorage.setItem('requests', JSON.stringify(requests.slice(-200)))
+    } catch (error) {
+      // ignore
+    }
+    return response
+  },
+  async error => {
+    try {
+      const requests = [
+        ...JSON.parse(sessionStorage.getItem('requests') || '[]'),
+        {
+          date: new Date().toISOString(),
+          ref: location.href,
+          ...error
+        }
+      ]
+      sessionStorage.setItem('requests', JSON.stringify(requests.slice(-200)))
+    } catch (error) {
+      // ignore
+    }
     if (!error.response) return Promise.reject(error)
     const { config, response: { status, data } } = error
-   if (status === 401 && data?.details?.errorMessage !== 'SESSION_PASSWORD_NEEDED') {
-     try {
+    if (status === 401 && data?.details?.errorMessage !== 'SESSION_PASSWORD_NEEDED') {
+      try {
        await req.post('/auth/refreshToken')
      } catch (error) {
        return Promise.reject(error)
