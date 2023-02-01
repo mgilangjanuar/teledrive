259755cab11729d2aed9173ddfa3e1950d6421c4
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-02-01 16:08:18 -0600

    Update Fetcher.ts

--- web/src/utils/Fetcher.ts
+++ web/src/utils/Fetcher.ts
@@ -2,77 +2,76 @@ import axios from 'axios'
 import { RETRY_COUNT } from './Constant'
 export const apiUrl = `${localStorage.getItem('API_URL') || process.env.REACT_APP_API_URL || ''}/api/v1`
 export const req = axios.create({
-  baseURL: apiUrl,
-  withCredentials: true
+ baseURL: apiUrl,
+ withCredentials: true
 })
-req.interceptors.response.use(response => {
-  try {
-    const requests = [...JSON.parse(sessionStorage.getItem('requests') || '[]'), {
-      date: new Date().toISOString(), ref: location.href, ...response
-    }]
-    sessionStorage.setItem('requests', JSON.stringify(requests.slice(-200)))
-  } catch (error) {
-    console.log(error)   // Add error logging
-  }
-  return response
-}, async error => {
-  try {
-    const requests = [...JSON.parse(sessionStorage.getItem('requests') || '[]'), {
-      date: new Date().toISOString(), ref: location.href, ...error
-    }]
-    sessionStorage.setItem('requests', JSON.stringify(requests.slice(-200)))
-  } catch (error) {
-    console.log(error)  // Add error logging
-  }
-  if (!error.response) {
-    throw error
-  }
-   const { config, response: { status, data } } = error
-  if (status === 401 && data?.details?.errorMessage !== 'SESSION_PASSWORD_NEEDED') {
-    try {
-      await req.post('/auth/refreshToken')
-    } catch (_error) {
-      throw error
-    }
-    try {
-      return await req(config)
-    } catch (err) {  // Add error handling for request error
-      throw err
-    }
-  } else if (status === 429) {
-    try {
-      await new Promise(res => setTimeout(res, data.retryAfter || 1000))
-      return await req(config)
-    } catch (err) {  // Add error handling for setTimeout error
-      throw err
-    }
-  } else if (status > 500) {
-    config.headers = {
-      ...config?.headers || {},
-      'x-retry-count': config.headers['x-retry-count'] || 0
-    }
+req.interceptors.response.use(
+ response => {
+   try {
+     const requests = [
+       ...JSON.parse(sessionStorage.getItem('requests') || '[]'),
+       {
+         date: new Date().toISOString(),
+         ref: location.href,
+         ...response
+       }
+     ]
+     sessionStorage.setItem('requests', JSON.stringify(requests.slice(-200)))
+   } catch (error) {
+     // ignore
+   }
+   return response
+ },
+ async error => {
+   try {
+     const requests = [
+       ...JSON.parse(sessionStorage.getItem('requests') || '[]'),
+       {
+         date: new Date().toISOString(),
+         ref: location.href,
+         ...error
+       }
+     ]
+     sessionStorage.setItem('requests', JSON.stringify(requests.slice(-200)))
+   } catch (error) {
+     // ignore
+   }
+    if (!error.response) return Promise.reject(error)
+    const { config, response: { status, data } } = error
+   if (status === 401 && data?.details?.errorMessage !== 'SESSION_PASSWORD_NEEDED') {
+     try {
+       await req.post('/auth/refreshToken')
+     } catch (error) {
+       return Promise.reject(error)
+     }
+     return req(config)
+   } else if (status === 429) {
+     await new Promise(resolve => setTimeout(resolve, data.retryAfter || 1000))
+     return req(config)
+   } else if (status > 500) {
+     config.headers = {
+       ...config?.headers || {},
+       'x-retry-count': config.headers['x-retry-count'] || 0
+     }
      if (config.headers['x-retry-count'] < RETRY_COUNT) {
-      try {
-        await new Promise(res => setTimeout(res, ++config.headers['x-retry-count'] * 3000))
-        return await req(config)
-      } catch (err) {  // Add error handling for setTimeout error
-        throw err
-      }
-    }
-  }
-  throw error
-})
- export const fetcher = async (url: string, authorization?: string): Promise<any> => {
-  const fetch = async () => {
-    const { data } = await req.get(url, {
-      ...authorization ? { headers: { authorization: `Bearer ${authorization}` } } : {},
-      withCredentials: true 
-    })
-    return data
-  };
+       await new Promise(resolve => setTimeout(resolve, ++config.headers['x-retry-count'] * 3000))
+       return req(config)
+     }
+   }
+   return Promise.reject(error)
+ }
+)
+export const fetcher = async (url: string, authorization?: string): Promise<any> => {
+ const fetch = async () => {
+   const { data } = await req.get(url, {
+     ...authorization ? { headers: { authorization: `Bearer ${authorization}` } } : {},
+     withCredentials: true
+   })
+   return data
+ }
   try {
-    return await fetch()
-  } catch ({ response }) {
-    throw response
-  }
+   return await fetch()
+ } catch ({ response }) {
+   throw response
+ }
 }
