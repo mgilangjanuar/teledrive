5037c2ddd69ec1cd862306582f39ea9e25e0cf4a
WitherTick <87392380+WitherTick@users.noreply.github.com>
2023-03-27 18:12:36 +0200

    feat: file rename and upload check
    This commit prevents uploading or renaming a file with the same name as a file already in the folder.
    Also, the confirmation button of the "rename form" is changed from "Add" to "Rename".

--- web/src/pages/dashboard/components/Rename.tsx
+++ web/src/pages/dashboard/components/Rename.tsx
@@ -25,32 +25,40 @@ const Rename: React.FC<Props> = ({
 
   const renameFile = async () => {
     setLoadingRename(true)
-    const { name } = formRename.getFieldsValue()
+    const name = String(formRename.getFieldsValue().name)
     try {
+      const { data: exists } = await req.get('/files', { params: { parent_id: fileRename.parent_id, name: name } })
+      if (/\.part0*\d*$/.test(name))
+        throw { status: 400, body: { error: 'The file name cannot end with ".part", even if followed by digits!' } }
+      if (/\(\d+\).+/.test(name))
+        throw { status: 400, body: { error: 'The file name cannot contain text after parentheses with digits inside!' } }
+      if (exists.length > 0)
+        throw { status: 400, body: { error: `A file/folder named "${name}" already exists!` } }
+
       const { data: result } = await req.patch(`/files/${fileRename?.id}`, {
         file: { name }
       })
       notification.success({
         message: 'Success',
-        description: `${name} renamed successfully!`
+        description: `${fileRename?.name.replace(/\.part0*\d+$/, '')} renamed successfully!`
       })
       dataSource?.[1](dataSource?.[0].map((datum: any) => datum.id === result.file.id ? { ...datum, name } : datum))
       setFileRename(undefined)
       setLoadingRename(false)
       formRename.resetFields()
       onFinish?.()
-    } catch (error) {
+    } catch (error: any) {
       setLoadingRename(false)
       return notification.error({
         message: 'Error',
-        description: 'Failed to rename a file. Please try again!'
+        description: error?.body?.error || 'Failed to rename a file. Please try again!'
       })
     }
   }
 
   return <Modal visible={fileRename}
     onCancel={() => setFileRename(undefined)}
-    okText="Add"
+    okText="Rename"
     title={<Typography.Text ellipsis>Rename {fileRename?.name.replace(/\.part0*\d+$/, '')}</Typography.Text>}
     onOk={() => formRename.submit()}
     cancelButtonProps={{ shape: 'round' }}
