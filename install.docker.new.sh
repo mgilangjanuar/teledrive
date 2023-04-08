a37456b58f523ef6c20e5d7318b965b9a897d327
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-04-08 11:42:56 -0700

    Update install.docker.new.sh

--- install.docker.new.sh
+++ install.docker.new.sh
@@ -16,12 +16,13 @@ export BUILDKIT_INLINE_CACHE=1
 export BUILDKIT_ENABLE_LEGACY_GIT=0
 
 # Check if the current user has permission to modify the necessary directories and files
-if [ ! -w /var/run/docker.sock ] || [ ! -w "$(pwd)/docker/.env" ] || [ ! -w "$(pwd)/docker/data" ]; then
+if [ ! -w /var/run/docker.sock ] || [ ! -w ./docker/.env ] || [ ! -w ./docker/data ]; then
   echo "This script requires root privileges to modify some files and directories."
-  exit 1
+  sudo -v
+  echo "Thanks!"
 fi
 
-if [ ! -f "$(pwd)/docker/.env" ]; then
+if [ ! -f docker/.env ]; then
   echo "Generating .env file..."
   ENV="develop"
   echo "Preparing your keys from https://my.telegram.org/"
@@ -34,23 +35,23 @@ if [ ! -f "$(pwd)/docker/.env" ]; then
   DB_PASSWORD=$(openssl rand -hex 16)
   echo "Generated random DB_PASSWORD: $DB_PASSWORD"
   echo
-  echo "ENV=$ENV" > "$(pwd)/docker/.env"
-  echo "PORT=$PORT" >> "$(pwd)/docker/.env"
-  echo "TG_API_ID=$TG_API_ID" >> "$(pwd)/docker/.env"
-  echo "TG_API_HASH=$TG_API_HASH" >> "$(pwd)/docker/.env"
-  echo "ADMIN_USERNAME=$ADMIN_USERNAME" >> "$(pwd)/docker/.env"
+  echo "ENV=$ENV" > docker/.env
+  echo "PORT=$PORT" >> docker/.env
+  echo "TG_API_ID=$TG_API_ID" >> docker/.env
+  echo "TG_API_HASH=$TG_API_HASH" >> docker/.env
+  echo "ADMIN_USERNAME=$ADMIN_USERNAME" >> docker/.env
   export DATABASE_URL=postgresql://postgres:$DB_PASSWORD@db:5432/teledrive
-  echo "DB_PASSWORD=$DB_PASSWORD" >> "$(pwd)/docker/.env"
-  if [ ! -d "$(pwd)/docker/data" ]; then
-    mkdir -p "$(pwd)/docker/data"
-    chown -R $(whoami):$(whoami) "$(pwd)/docker"
-    chmod -R 777 "$(pwd)/docker"
+  echo "DB_PASSWORD=$DB_PASSWORD" >> docker/.env
+  if [ ! -d "docker/data" ]; then
+    mkdir -p docker/data
+    chown -R $(whoami):$(whoami) docker
+    chmod -R 777 docker
   fi
   cd docker
-  echo $DB_PASSWORD | sudo -S docker compose build teledrive
-  echo $DB_PASSWORD | sudo -S docker compose up -d
+  docker compose build teledrive
+  docker compose up -d
   sleep 2
-  echo $DB_PASSWORD | sudo -S docker compose exec teledrive yarn workspace api prisma migrate deploy
+  docker compose exec teledrive yarn workspace api prisma migrate deploy
 else
   cd docker
   git fetch origin
@@ -58,12 +59,12 @@ else
     git branch staging origin/staging
   fi
   git checkout staging
-  export $(cat "$(pwd)/docker/.env" | xargs)
-  echo $DB_PASSWORD | sudo -S docker compose down
-  echo $DB_PASSWORD | sudo -S docker compose up --build --force-recreate -d
+  export $(cat docker/.env | xargs)
+  docker compose down
+  docker compose up --build --force-recreate -d
   sleep 2
-  echo $DB_PASSWORD | sudo -S docker compose up -d
-  echo $DB_PASSWORD | sudo -S docker compose exec teledrive yarn workspace api prisma migrate deploy
+  docker compose up -d
+  docker compose exec teledrive yarn workspace api prisma migrate deploy
   git reset --hard
   git clean -f
   git pull origin staging
