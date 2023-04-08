db8051ebde19c627feacd2e2c1fee380b48f1de8
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-04-08 12:56:27 -0700

    Experimental connection pool implementation

--- web/src/utils/Download.ts
+++ web/src/utils/Download.ts
@@ -1,83 +1,70 @@
 import streamSaver from 'streamsaver'
 import { Api } from 'telegram'
-import { req } from './Fetcher'
 import { telegramClient } from './Telegram'
 
-export async function download(id: string): Promise<ReadableStream> {
-  const { data: response } = await req.get(`/files/${id}`, {
-    params: { raw: 1, as_array: 1 }
-  })
-  let cancel = false
-  const client = await telegramClient.connect()
+class ConnectionPool {
+  private connections: Promise<any>[]
 
-  const fileIterator = {
-    [Symbol.asyncIterator]: async function* () {
-      for (const file of response.files) {
-        let chat: any
-        if (file.forward_info && file.forward_info.match(/^channel\//gi)) {
-          const [type, peerId, id, accessHash] = file.forward_info.split('/')
-          let peer: Api.InputPeerChannel | Api.InputPeerUser | Api.InputPeerChat
-          if (type === 'channel') {
-            peer = new Api.InputPeerChannel({
-              channelId: BigInt(peerId) as any,
-              accessHash: BigInt(accessHash as string) as any
-            })
-            chat = await client.invoke(
-              new Api.channels.GetMessages({
-                channel: peer,
-                id: [new Api.InputMessageID({ id: Number(id) })]
-              })
-            )
-          }
-        } else {
-          chat = await client.invoke(
-            new Api.messages.GetMessages({
-              id: [new Api.InputMessageID({ id: Number(file.message_id) })]
-            })
-          )
-        }
-        yield { chat, media: chat['messages'][0].media }
-      }
-    }
+  constructor() {
+    this.connections = []
   }
 
-  const readableStream = new ReadableStream({
-    start(_controller: ReadableStreamDefaultController) {},
-    async pull(controller: ReadableByteStreamController) {
-      const getData = async (chat: any, media: any) => {
-        const chunks = await client.downloadMedia(media)
-        for (const chunk of chunks) {
-          if (cancel) return false
-          controller.enqueue(chunk)
-        }
-      }
-      const downloadPromises = []
-      for await (const { chat, media } of fileIterator) {
-        downloadPromises.push(getData(chat, media))
-      }
-      await Promise.all(downloadPromises)
-      controller.close()
-    },
-    cancel() {
-      cancel = true
+  async getConnection() {
+    if (this.connections.length > 0) {
+      return this.connections.shift()
     }
-  })
+    const connection = telegramClient.connect()
+    this.connections.push(connection)
+    return connection
+  }
 
-  return readableStream
+  releaseConnection(connection: Promise<any>) {
+    this.connections.push(connection)
+  }
 }
 
-export const directDownload = async (
-  id: string,
-  name: string
-): Promise<void> => {
+const connectionPool = new ConnectionPool()
+
+export async function download(id: string): Promise<ReadableStream> {
+  const client = await connectionPool.getConnection()
+
+  try {
+    const { data: response } = await client.invoke(new Api.messages.GetMessages({ id: [new Api.InputMessageID({ id: Number(id) })] }))
+    const media = response.messages[0].media
+    const chunks = await client.downloadMedia(media)
+
+    const buffer = new Uint8Array(chunks.reduce((acc, chunk) => acc + chunk.byteLength, 0))
+    let offset = 0
+    for (const chunk of chunks) {
+      buffer.set(new Uint8Array(chunk), offset)
+      offset += chunk.byteLength
+    }
+
+    return new ReadableStream({
+      start(controller) {
+        controller.enqueue(buffer)
+        controller.close()
+      }
+    })
+  } finally {
+    connectionPool.releaseConnection(client)
+  }
+}
+
+export const directDownload = async (id: string, name: string): Promise<void> => {
   const fileStream = streamSaver.createWriteStream(name)
   const writer = fileStream.getWriter()
   const reader = (await download(id)).getReader()
-  const pump = () =>
-    reader.read().then(({ value, done }) => {
-      if (done) return writer.close()
-      writer.write(value)
-      return writer.ready.then(pump)
-    })
+
+  const pump = async () => {
+    const { done, value } = await reader.read()
+    if (done) {
+      writer.close()
+      return
+    }
+    writer.write(value)
+    await pump()
+  }
+
   await pump()
 }
