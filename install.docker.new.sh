ff516b1433a2c372078265d9a6cc55dda779ab37
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-04-08 10:22:16 -0700

    Fix Docker data path

--- install.docker.new.sh
+++ install.docker.new.sh
@@ -1,39 +1,43 @@
 #!/bin/bash
- set -e
- export NODE_OPTIONS="--openssl-legacy-provider --no-experimental-fetch"
- echo "Node Version: $(node -v)"
+
+set -e
+
+export NODE_OPTIONS="--openssl-legacy-provider --no-experimental-fetch"
+
+echo "Node Version: $(node -v)"
 echo "cURL Version: $(curl --version | head -n 1)"
 echo "Docker Version: $(docker -v)"
 echo "Docker Compose Version: $(docker compose version)"
- if [ ! -f docker/.env ]
-then
+
+if [ ! -f docker/.env ]; then
   echo "Generating .env file..."
-   ENV="develop"
-   echo "Preparing your keys from https://my.telegram.org/"
+  ENV="develop"
+  echo "Preparing your keys from https://my.telegram.org/"
   read -p "Enter your TG_API_ID: " TG_API_ID
   read -p "Enter your TG_API_HASH: " TG_API_HASH
-   echo
+  echo
   read -p "Enter your ADMIN_USERNAME: " ADMIN_USERNAME
   read -p "Enter your PORT: " PORT
   PORT="${PORT:=4000}"
-   read -p "Enter your DB_PASSWORD: " DB_PASSWORD
+  read -p "Enter your DB_PASSWORD: " DB_PASSWORD
   echo
-   echo "ENV=$ENV" > docker/.env
+  echo "ENV=$ENV" > docker/.env
   echo "PORT=$PORT" >> docker/.env
   echo "TG_API_ID=$TG_API_ID" >> docker/.env
   echo "TG_API_HASH=$TG_API_HASH" >> docker/.env
   echo "ADMIN_USERNAME=$ADMIN_USERNAME" >> docker/.env
   export DATABASE_URL=postgresql://postgres:$DB_PASSWORD@db:5432/teledrive
   echo "DB_PASSWORD=$DB_PASSWORD" >> docker/.env
-   cd docker
+  mkdir -p docker/data
+  cd docker
   docker compose build teledrive
   docker compose up -d
   sleep 2
   docker compose exec teledrive yarn workspace api prisma migrate deploy
 else
   git pull origin staging
-   export $(cat docker/.env | xargs)
-   cd docker
+  export $(cat docker/.env | xargs)
+  cd docker
   docker compose down
   docker compose up --build --force-recreate -d
   sleep 2
