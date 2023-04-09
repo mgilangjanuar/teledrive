45752c0a38b339e4150499a8ee26f13c764e42ca
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-04-09 17:48:09 -0500

    implement parallel processing experimental

--- web/src/utils/Download.ts
+++ web/src/utils/Download.ts
@@ -5,17 +5,30 @@ import { telegramClient } from './Telegram'
 class ConnectionPool {
   private connections: Promise<any>[]
 
-  constructor() {
+  constructor(maxSize: number) {
     this.connections = []
+    this.maxSize = maxSize
   }
 
   async getConnection() {
     if (this.connections.length > 0) {
       return this.connections.shift()
     }
-    const connection = telegramClient.connect()
-    this.connections.push(connection)
-    return connection
+    if (this.connections.length < this.maxSize) {
+      const connection = telegramClient.connect()
+      this.connections.push(connection)
+      return connection
+    }
+    return new Promise((resolve) => {
+      const interval = setInterval(() => {
+        if (this.connections.length < this.maxSize) {
+          const connection = telegramClient.connect()
+          this.connections.push(connection)
+          clearInterval(interval)
+          resolve(connection)
+        }
+      }, 1000)
+    })
   }
 
   releaseConnection(connection: Promise<any>) {
@@ -23,9 +36,24 @@ class ConnectionPool {
   }
 }
 
-const connectionPool = new ConnectionPool()
+const connectionPool = new ConnectionPool(5) // set maximum pool size to 5
 
-export async function download(id: string): Promise<ReadableStream> {
+const cache = new Map<string, Uint8Array>() // create a cache for downloaded data
+
+export async function download(
+  id: string,
+  numParallel: number = 1
+): Promise<ReadableStream> {
+  const cachedData = cache.get(id)
+  if (cachedData) {
+    return new ReadableStream({
+      start(controller) {
+        controller.enqueue(cachedData)
+        controller.close()
+      }
+    })
+  }
+
   const client = await connectionPool.getConnection()
 
   try {
@@ -36,25 +64,56 @@ export async function download(id: string): Promise<ReadableStream> {
     )
     const media = response.messages[0].media
 
-    const fileIterator = {
-      [Symbol.asyncIterator]: async function* () {
-        const chunks = await client.downloadMedia(media)
-        for (const chunk of chunks) {
-          yield chunk
+    const fileIterators = []
+    for (let i = 0; i < numParallel; i++) {
+      fileIterators.push({
+        [Symbol.asyncIterator]: async function* () {
+          const chunks = await client.downloadMedia(media, {
+            offset: (i * media.size) / numParallel,
+            limit: media.size / numParallel
+          })
+          for (const chunk of chunks) {
+            yield chunk
+          }
         }
-      }
+      })
     }
 
-    return new ReadableStream({
+    const stream = new ReadableStream({
       start(controller) {
-        (async () => {
-          for await (const chunk of fileIterator) {
-            controller.enqueue(chunk)
+        const readers = []
+        for (const fileIterator of fileIterators) {
+          readers.push(fileIterator[Symbol.asyncIterator]().getReader())
+        }
+
+        const pump = async () => {
+          const promises = []
+          for (let i = 0; i < readers.length; i++) {
+            const reader = readers[i]
+            const promise = reader.read()
+            promises.push(promise)
+            promise.then(({ done, value }) => {
+              if (done) {
+                readers[i] = null
+              } else {
+                controller.enqueue(value)
+              }
+            })
           }
-          controller.close()
-        })()
+          await Promise.all(promises)
+          if (readers.every((reader) => reader === null)) {
+            cache.set(id, controller.byobRequest.buffer)
+            controller.close()
+          } else {
+            await pump()
+          }
+        }
+
+        pump()
       }
     })
+
+    return stream
   } finally {
     connectionPool.releaseConnection(client)
   }
@@ -62,21 +121,42 @@ export async function download(id: string): Promise<ReadableStream> {
 
 export const directDownload = async (
   id: string,
-  name: string
+  name: string,
+  numParallel: number = 1
 ): Promise<void> => {
   const fileStream = streamSaver.createWriteStream(name)
   const writer = fileStream.getWriter()
-  const reader = (await download(id)).getReader()
 
-  const pump = async () => {
-    const { done, value } = await reader.read()
-    if (done) {
-      writer.close()
-      return
+  try {
+    const readers = []
+    for (let i = 0; i < numParallel; i++) {
+      readers.push((await download(id, numParallel)).getReader())
     }
-    writer.write(value)
-    await pump()
-  }
 
-  await pump()
+    const pump = async () => {
+      const promises = []
+      for (let i = 0; i < readers.length; i++) {
+        const reader = readers[i]
+        const promise = reader.read()
+        promises.push(promise)
+        promise.then(({ done, value }) => {
+          if (done) {
+            readers[i] = null
+          } else {
+            writer.write(value)
+          }
+        })
+      }
+      await Promise.all(promises)
+      if (readers.every((reader) => reader === null)) {
+        writer.close()
+      } else {
+        await pump()
+      }
+    }
+
+    await pump()
+  } catch (error) {
+    console.error(error)
+  }
 }
