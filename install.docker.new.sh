49f42110101d540f3c07a0265b364a96f66416f9
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-04-14 14:50:02 -0500

    Update install.docker.new.sh

--- install.docker.new.sh
+++ install.docker.new.sh
@@ -4,7 +4,7 @@ set -e
 
 # Create /docker directory if it doesn't exist
 if [ ! -d docker ]; then
-mkdir docker
+  mkdir docker
 fi
 
 # Configure Node.js and cURL
@@ -24,38 +24,49 @@ export BUILDKIT_ENABLE_LEGACY_GIT=0
 
 # Generate .env file if it doesn't exist
 if [ ! -f docker/.env ]; then
-printf "Generating .env file...\n"
-ENV="develop"
-read -p "Enter your TG_API_ID: " TG_API_ID
-read -p "Enter your TG_API_HASH: " TG_API_HASH
-printf "\n"
-read -p "Enter your ADMIN_USERNAME: " ADMIN_USERNAME
-read -p "Enter your PORT [4000]: " PORT
-PORT="${PORT:-4000}"
-DB_PASSWORD=$(openssl rand -hex 16)
-printf "Generated random DB_PASSWORD: %s\n" "$DB_PASSWORD"
-printf "ENV=%s\nPORT=%s\nTG_API_ID=%s\nTG_API_HASH=%s\nADMIN_USERNAME=%s\nDB_PASSWORD=%s\n" \
-"$ENV" "$PORT" "$TG_API_ID" "$TG_API_HASH" "$ADMIN_USERNAME" "$DB_PASSWORD" > docker/.env
-fi
+  printf "Generating .env file...\n"
+  ENV="develop"
+  read -p "Enter your TG_API_ID: " TG_API_ID
+  read -p "Enter your TG_API_HASH: " TG_API_HASH
+  printf "\n"
+  read -p "Enter your ADMIN_USERNAME: " ADMIN_USERNAME
+  read -p "Enter your PORT [4000]: " PORT
+  PORT="${PORT:-4000}"
+  DB_PASSWORD=$(openssl rand -hex 16)
 
-# Check if the current user has permission to modify the necessary directories and files
-if [ ! -w /var/run/docker.sock ] || [ ! -w ./docker/.env ] || [ ! -w ./docker/data ]; then
-printf "This script requires permission to modify some files and directories.\n"
-printf "Giving permission to the current user...\n"
-sudo chown -R "$(whoami)" /var/run/docker.sock ./docker/.env ./docker/data
-fi
+  # Stop and start services using Docker Compose
+  docker compose down
+  docker compose up --build --force-recreate -d
+  sleep 2
+  docker compose exec teledrive yarn workspace api prisma migrate deploy  
 
-# Build and start services using Docker Compose
-cd docker
-if ! git branch --list experiment >/dev/null; then
-git branch experiment origin/experiment
+  # Update PostgreSQL password
+  docker compose exec docker-db-1 psql -U postgres -c "ALTER USER postgres PASSWORD '${DB_PASSWORD}';"
+
+   printf "Generated random DB_PASSWORD: %s\n" "$DB_PASSWORD"
+  printf "ENV=%s\nPORT=%s\nTG_API_ID=%s\nTG_API_HASH=%s\nADMIN_USERNAME=%s\nDB_PASSWORD=%s\n" \
+  "$ENV" "$PORT" "$TG_API_ID" "$TG_API_HASH" "$ADMIN_USERNAME" "$DB_PASSWORD" > docker/.env
+
+else
+  # Check if the current user has permission to modify the necessary directories and files
+  if [ ! -w /var/run/docker.sock ] || [ ! -w ./docker/.env ] || [ ! -w ./docker/data ]; then
+    printf "This script requires permission to modify some files and directories.\n"
+    printf "Giving permission to the current user...\n"
+    sudo chown -R "$(whoami)" /var/run/docker.sock ./docker/.env ./docker/data
+  fi
+
+  # Build and start services using Docker Compose
+  cd docker
+  if ! git branch --list experiment >/dev/null; then
+    git branch experiment origin/experiment
+  fi
+  git checkout experiment
+  export $(grep -v '^#' docker/.env | xargs)
+  docker compose down
+  docker compose up --build --force-recreate -d
+  sleep 2
+  docker compose exec teledrive yarn workspace api prisma migrate deploy
+  git reset --hard
+  git clean -f
+  git pull origin experiment
 fi
-git checkout experiment
-export $(grep -v '^#' docker/.env | xargs)
-docker compose down
-docker compose up --build --force-recreate -d
-sleep 2
-docker compose exec teledrive yarn workspace api prisma migrate deploy
-git reset --hard
-git clean -f
-git pull origin experiment
