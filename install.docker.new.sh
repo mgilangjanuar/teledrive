5b7a507042c003b8426cb71956762df03a8f93a9
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-04-14 14:13:33 -0500

    Update install.docker.new.sh

--- install.docker.new.sh
+++ install.docker.new.sh
@@ -2,14 +2,16 @@
 
 set -e
 
+# Configure Node.js and cURL
 export NODE_OPTIONS="--openssl-legacy-provider --no-experimental-fetch"
+printf "Node.js Version: %s\n" "$(node -v)"
+printf "cURL Version: %s\n" "$(curl --version | head -n 1)"
 
-echo "Node Version: $(node -v)"
-echo "cURL Version: $(curl --version | head -n 1)"
-echo "Docker Version: $(docker -v)"
-echo "Docker Compose Version: $(docker compose version)"
+# Check Docker and Docker Compose versions
+printf "Docker Version: %s\n" "$(docker -v)"
+printf "Docker Compose Version: %s\n" "$(docker compose -v)"
 
-# Disable Git-related functionality in buildx
+# Disable Git-related functionality in BuildKit
 export DOCKER_BUILDKIT=1
 export BUILDKIT_PROGRESS=plain
 export BUILDKIT_INLINE_CACHE=1
@@ -17,63 +19,44 @@ export BUILDKIT_ENABLE_LEGACY_GIT=0
 
 # Check if the current user has permission to modify the necessary directories and files
 if [ ! -w /var/run/docker.sock ] || [ ! -w ./docker/.env ] || [ ! -w ./docker/data ]; then
-  echo "This script requires root privileges to modify some files and directories."
-  if sudo -n true 2>/dev/null; then
-    sudo -E bash -c "
-      echo 'Thanks!'
-      $0
-    "
-    exit
-  else
-    echo "Please run the script with sudo."
-    exit 1
-  fi
+printf "This script requires root privileges to modify some files and directories.\n"
+if sudo -E true; then
+printf "Thanks!\n"
+sudo -E "$0"
+exit
+else
+printf "Please run the script with sudo.\n"
+exit 1
+fi
 fi
 
+# Generate .env file if it doesn't exist
 if [ ! -f docker/.env ]; then
-  echo "Generating .env file..."
-  ENV="develop"
-  echo "Preparing your keys from https://my.telegram.org/"
-  read -p "Enter your TG_API_ID: " TG_API_ID
-  read -p "Enter your TG_API_HASH: " TG_API_HASH
-  echo
-  read -p "Enter your ADMIN_USERNAME: " ADMIN_USERNAME
-  read -p "Enter your PORT: " PORT
-  PORT="${PORT:=4000}"
-  DB_PASSWORD=$(openssl rand -hex 16)
-  echo "Generated random DB_PASSWORD: $DB_PASSWORD"
-  echo
-  echo "ENV=$ENV" > docker/.env
-  echo "PORT=$PORT" >> docker/.env
-  echo "TG_API_ID=$TG_API_ID" >> docker/.env
-  echo "TG_API_HASH=$TG_API_HASH" >> docker/.env
-  echo "ADMIN_USERNAME=$ADMIN_USERNAME" >> docker/.env
-  export DATABASE_URL=postgresql://postgres:$DB_PASSWORD@db:5432/teledrive
-  echo "DB_PASSWORD=$DB_PASSWORD" >> docker/.env
-  if [ ! -d "docker/data" ]; then
-    mkdir -p docker/data
-    chown -R $(whoami):$(whoami) docker
-    chmod -R 777 docker
-  fi
-  cd docker
-  docker compose build teledrive
-  docker compose up -d
-  sleep 2
-  docker compose exec teledrive yarn workspace api prisma migrate deploy
-else
-  cd docker
-  git fetch origin
-  if ! git rev-parse --verify experiment >/dev/null 2>&1; then
-    git branch experiment origin/experiment
-  fi
-  git checkout experiment
-  export $(cat docker/.env | xargs)
-  docker compose down
-  docker compose up --build --force-recreate -d
-  sleep 2
-  docker compose up -d
-  docker compose exec teledrive yarn workspace api prisma migrate deploy
-  git reset --hard
-  git clean -f
-  git pull origin experiment
+printf "Generating .env file...\n"
+ENV="develop"
+read -p "Enter your TG_API_ID: " TG_API_ID
+read -p "Enter your TG_API_HASH: " TG_API_HASH
+printf "\n"
+read -p "Enter your ADMIN_USERNAME: " ADMIN_USERNAME
+read -p "Enter your PORT [4000]: " PORT
+PORT="${PORT:-4000}"
+DB_PASSWORD=$(openssl rand -hex 16)
+printf "Generated random DB_PASSWORD: %s\n" "$DB_PASSWORD"
+printf "ENV=%s\nPORT=%s\nTG_API_ID=%s\nTG_API_HASH=%s\nADMIN_USERNAME=%s\nDB_PASSWORD=%s\n" \
+"$ENV" "$PORT" "$TG_API_ID" "$TG_API_HASH" "$ADMIN_USERNAME" "$DB_PASSWORD" > docker/.env
 fi
+
+# Build and start services using Docker Compose
+cd docker
+if ! git branch --list experiment >/dev/null; then
+git branch experiment origin/experiment
+fi
+git checkout experiment
+export $(grep -v '^#' docker/.env | xargs)
+docker compose down
+docker compose up --build --force-recreate -d
+sleep 2
+docker compose exec teledrive yarn workspace api prisma migrate deploy
+git reset --hard
+git clean -f
+git pull origin experiment
