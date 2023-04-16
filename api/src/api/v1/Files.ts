0e768ee5bc45c38302c5d582bb9cd2f1fecb15d5
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-04-16 14:23:09 -0500

    Update Files.ts

--- api/src/api/v1/Files.ts
+++ api/src/api/v1/Files.ts
@@ -5,7 +5,7 @@ import checkDiskSpace from 'check-disk-space'
 import contentDisposition from 'content-disposition'
 import { AES, enc } from 'crypto-js'
 import { Request, Response } from 'express'
-import { appendFileSync, createReadStream, existsSync, mkdirSync, readdirSync, renameSync, rmSync, statSync, writeFileSync } from 'fs'
+import { createWriteStream, appendFileSync, createReadStream, existsSync, mkdirSync, readdirSync, renameSync, rmSync, statSync, writeFileSync } from 'fs'
 import moment from 'moment'
 import multer from 'multer'
 import { Api, Logger, TelegramClient } from 'telegram'
@@ -1290,16 +1290,16 @@ export class Files {
 
       const getData = async () => {
         try {
-          await req.tg.downloadMedia(chat['messages'][0].media, {
-            ...thumb ? { thumb: 0 } : {},
-            outputFile,
-          }, (progress) => {
+          await req.tg.downloadMedia(chat['messages'][0].media, (progress) => {
             downloaded = progress
             if (cancel) {
               throw { status: 422, body: { error: 'canceled' } }
             } else {
               console.log(`${chat['messages'][0].id} ${downloaded}/${chat['messages'][0].media.document.size.value} (${downloaded / Number(totalFileSize) * 100 + '%'})`)
             }
+          }, {
+            ...thumb ? { thumb: 0 } : {},
+            outputFile,
           })
         } catch (error) {
           console.log(error)
