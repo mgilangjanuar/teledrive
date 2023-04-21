b94fc10a56b2cff3ac0f9697afe122b5e21591a0
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-04-21 17:49:03 -0500

    add bit heaps and add divide and conquer algorithm

--- web/src/utils/Download.ts
+++ web/src/utils/Download.ts
@@ -33,30 +33,50 @@ class ConnectionPool {
     this.connections.push(connection)
   }
 }
-// Declare the proper type for file iterators
+// Declare the proper type for file iterators 
 type FileIterator = {
   [Symbol.asyncIterator]: () => AsyncGenerator<Uint8Array, void, unknown>
 }
-const connectionPool = new ConnectionPool(5) // set maximum pool size to 5
-const cache = new Map<string, Uint8Array>() // create a cache for downloaded data
+const connectionPool = new ConnectionPool(5) // set maximum pool size to 5 
+const cache = new Map<string, Uint8Array>() // create a cache for downloaded data 
 async function* generateChunks(
   clients: any[],
   media: any,
   i: number,
-  numParallel: number
+  numParallel: number,
+  bufferSize: number = 1024 * 1024 // set buffer size to 1 MB 
 ): AsyncGenerator<Uint8Array, void, unknown> {
   const numConnections = clients.length
   let connIndex = i % numConnections
   let offset = Math.floor(i / numConnections) * media.size / numParallel
   const limit = media.size / numParallel
+  const promises: Promise<Uint8Array[]>[] = []
+  let buffer: Uint8Array[] = []
+  let bufferLength = 0
   for (let j = 0; j < numParallel; j++) {
     const client = clients[connIndex]
-    const chunks = await client.downloadMedia(media, { offset, limit })
-    for (const chunk of chunks) {
-      yield chunk
-    }
+    promises.push(
+      client.downloadMedia(media, { offset, limit })
+    )
     offset += limit
     connIndex = (connIndex + 1) % numConnections
+  }
+  const chunksArray = await Promise.all(promises)
+  for (const chunks of chunksArray) {
+    for (const chunk of chunks) {
+      buffer.push(chunk)
+      bufferLength += chunk.byteLength
+      if (bufferLength >= bufferSize) {
+        const concatenated = concat(buffer)
+        yield concatenated
+        buffer = []
+        bufferLength = 0
+      }
+    }
+  }
+  if (bufferLength > 0) {
+    const concatenated = concat(buffer)
+    yield concatenated
   }
 }
 export async function download(
@@ -123,13 +143,13 @@ export const directDownload = async (
   const writer = fileStream.getWriter()
   try {
     const streams = await download(id, numParallel)
-    // Combine the streams using a function like mergeStreams in place of [stream1, stream2, ..., streamN]
+    // Combine the streams using a function like mergeStreams in place of [stream1, stream2, ..., streamN] 
     const mergedStream = mergeStreams(streams)
     const reader = mergedStream.getReader()
     const pump = async () => {
       const { done, value } = await reader.read()
       if (done) {
-        if (value) { // add null check here
+        if (value) { // add null check here 
           cache.set(id, value)
         }
         writer.close()
@@ -143,18 +163,67 @@ export const directDownload = async (
     console.error(error)
   }
 }
+
 export function mergeStreams(...streams: ReadableStream<Uint8Array>[]): ReadableStream<Uint8Array> {
+  if (streams.length === 1) {
+    return streams[0]
+  }
+
+  const mid = Math.floor(streams.length / 2)
+  const left = mergeStreams(...streams.slice(0, mid))
+  const right = mergeStreams(...streams.slice(mid))
+
+  const heap: [number, ReadableStreamDefaultReader<Uint8Array>][] = []
+
+  function heapify<T>(arr: [number, T][]): void {
+    for (let i = Math.floor(arr.length / 2); i >= 0; i--) {
+      siftDown(arr, i)
+    }
+  }
+
+  function siftDown<T>(arr: [number, T][], idx: number): void {
+    const left = 2 * idx + 1
+    const right = 2 * idx + 2
+    let maxIdx = idx
+    if (left < arr.length && arr[left][0] > arr[maxIdx][0]) {
+      maxIdx = left
+    }
+    if (right < arr.length && arr[right][0] > arr[maxIdx][0]) {
+      maxIdx = right
+    }
+    if (maxIdx !== idx) {
+      [arr[idx], arr[maxIdx]] = [arr[maxIdx], arr[idx]]
+      siftDown(arr, maxIdx)
+    }
+  }
+
+  // Initialize heap with first chunk from each stream 
+  const leftReader = left.getReader()
+  const rightReader = right.getReader()
+  const read = async (reader: ReadableStreamDefaultReader<Uint8Array>) => {
+    const { done, value } = await reader.read()
+    if (!done && value !== undefined) {
+      heap.push([value.byteLength, reader])
+      heapify(heap)
+    }
+  }
+  read(leftReader)
+  read(rightReader)
+
   const combinedStream = new ReadableStream({
     async start(controller) {
-      let allDone = false
-      while (!allDone) {
-        allDone = true
-        for (const stream of streams) {
-          const reader = stream.getReader()
-          const { done, value } = await reader.read()
-          if (!done) {
-            allDone = false
+      while (heap.length > 0) {
+        const [_, reader] = heap[0]
+        const { done, value } = await reader.read()
+        if (done) {
+          heap[0] = heap[heap.length - 1]
+          heap.pop()
+          heapify(heap)
+        } else {
+          if (value !== undefined) {
             controller.enqueue(value)
+            heap[0] = [value.byteLength, reader]
+            heapify(heap)
           }
         }
       }
