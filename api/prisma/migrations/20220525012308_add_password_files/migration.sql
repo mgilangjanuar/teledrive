e577b10bae403dcb2f47ff898294c33ba02d43c7
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-04-22 17:50:23 -0500

    Update migration.sql

--- api/prisma/migrations/20220525012308_add_password_files/migration.sql
+++ api/prisma/migrations/20220525012308_add_password_files/migration.sql
@@ -1,26 +1,33 @@
 prisma.migrate({
-  // ...
-  steps: [
+  up: [
     {
       name: 'create_files_table',
-      up: `
+      sql: `
         CREATE TABLE files (
           id SERIAL PRIMARY KEY,
           name VARCHAR(255),
           path VARCHAR(255)
         );
       `,
-      down: `
-        DROP TABLE files;
-      `,
     },
     {
       name: 'add_password_field_to_files_table',
-      up: `
+      sql: `
         ALTER TABLE files ADD COLUMN password VARCHAR;
       `,
-      down: `
+    },
+  ],
+  down: [
+    {
+      name: 'drop_password_field_from_files_table',
+      sql: `
         ALTER TABLE files DROP COLUMN password;
+      `,
+    },
+    {
+      name: 'drop_files_table',
+      sql: `
+        DROP TABLE files;
       `,
     },
   ],
